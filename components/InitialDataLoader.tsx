fe7e1e720ec4318e5293577f7500bf9ca3844b4d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-08-03 19:47:57 +0900

    Update InitialDataLoader.tsx

--- components/InitialDataLoader.tsx
+++ components/InitialDataLoader.tsx
@@ -8,7 +8,7 @@ interface InitialDataLoaderProps {
 
 const InitialDataLoader: React.FC<InitialDataLoaderProps> = ({ onDataLoaded }) => {
     const loadInitialProgressData = useCallback(() => {
-        fetch('https://drive.google.com/file/d/1Tz6GgntkoXWg846roquzoa1ywPCENmmH/view?usp=drive_link')
+        fetch('https://drive.google.com/uc?export=download&id=1Tz6GgntkoXWg846roquzoa1ywPCENmmH')
             .then((response) => response.text())
             .then((csvText) => {
                 Papa.parse(csvText, {
